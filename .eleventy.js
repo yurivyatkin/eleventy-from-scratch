b7544a80c33367279beb8600934cdc3226084b0f
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2021-08-24 20:20:53 +1200

    [#8] Configure work collection

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,15 @@
 module.exports = (config) => {
   config.addPassthroughCopy('./src/images/');
+
+  // Returns work items, sorted by display order
+  config.addCollection('work', (collection) => {
+    return collection
+      .getFilteredByGlob('./src/work/*.md')
+      .sort((a, b) =>
+        Number(a.data.displayOrder) > Number(b.data.displayOrder) ? 1 : -1
+      );
+  });
+
   return {
     markdownTemplateEngine: 'njk',
     dataTemplateEngine: 'njk',
