34cf351798a56dd2f97f8b4c1abcbb51905cf2f5
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2021-08-24 20:24:15 +1200

    [#8] Configure featuredWork collection

--- .eleventy.js
+++ .eleventy.js
@@ -9,6 +9,13 @@ module.exports = (config) => {
         Number(a.data.displayOrder) > Number(b.data.displayOrder) ? 1 : -1
       );
   });
+  // same as above but additionally filtered by the featured flag
+  config.addCollection('featuredWork', collection => {
+    return collection
+      .getFilteredByGlob('./src/work/*.md')
+      .sort((a, b) => (Number(a.data.displayOrder) > Number(b.data.displayOrder) ? 1 : -1))
+      .filter(x => x.data.featured);
+  });
 
   return {
     markdownTemplateEngine: 'njk',
