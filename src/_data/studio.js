a5e8cba95b59838b6918c44a15b9658df57a0fde
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2021-08-24 21:32:09 +1200

    [#9] Replace fetch with cache

--- src/_data/studio.js
+++ src/_data/studio.js
@@ -1,14 +1,27 @@
-const fetch = require('node-fetch');
+const Cache = require('@11ty/eleventy-cache-assets');
 
+/**
+ * Grabs the remote data for studio images and returns back
+ * an array of objects
+ *
+ * @returns {Array} Empty or array of objects
+ */
 module.exports = async () => {
   try {
-    const res = await fetch(
-      'https://11ty-from-scratch-content-feeds.piccalil.li/media.json'
+    // Grabs either the fresh remote data or cached data (will always be fresh live)
+    const { items } = await Cache(
+      'https://11ty-from-scratch-content-feeds.piccalil.li/media.json',
+      {
+        duration: '1d', // 1 day
+        type: 'json',
+      }
     );
-    const { items } = await res.json();
+
     return items;
   } catch (ex) {
     console.log(ex);
+
+    // If failed, return back an empty array
     return [];
   }
 };
