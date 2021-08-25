3075c52d71da5dfc02c4e44785a4ac3cb9750e79
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2021-08-25 22:17:24 +1200

    [#11] Configure blog collection ... actually using posts folder.

--- .eleventy.js
+++ .eleventy.js
@@ -14,6 +14,11 @@ module.exports = (config) => {
     ).filter((x) => x.data.featured);
   });
 
+  // Returns a collection of blog posts in reverse date order
+  config.addCollection('blog', (collection) => {
+    return [...collection.getFilteredByGlob('./src/posts/*.md')].reverse();
+  });
+
   return {
     markdownTemplateEngine: 'njk',
     dataTemplateEngine: 'njk',
